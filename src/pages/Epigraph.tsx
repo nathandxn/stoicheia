d92b6da4efe023850840d10866442f07ca13a5d9
nathandxn <nathandxn@gmail.com>
2026-03-22 01:13:44 -0400

    feat: add epigraph

---
+++ src/pages/Epigraph.tsx
@@ -0,0 +1,12 @@
+import './Epigraph.css'
+
+export default function Epigraph() {
+  return (
+    <div className="epigraph">
+      <blockquote className="quote">
+        <p>Things which equal the same thing also equal one another.</p>
+        <cite>— Euclid's Elements, Common notion 1</cite>
+      </blockquote>
+    </div>
+  )
+}
